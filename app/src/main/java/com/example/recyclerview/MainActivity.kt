361791fb38d65bb3d0018ff372e77f015f55440c
KharinAA1986 <kharinaa1986@gmail.com>
2023-03-12 20:22:50 +0500

    Corrected colors

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,25 +1,24 @@
 package com.example.recyclerview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val adapter = CardAdapter()
     private val cardColor = listOf(
-        R.color.white,
-        R.color.black,
-        R.color.grey,
-        R.color.purple_200,
-        R.color.purple_500,
-        R.color.purple_700,
-        R.color.teal_200,
-        R.color.teal_700,)
-    var k:Int = R.color.purple_700
+        Color.WHITE,
+        Color.BLUE,
+        Color.GREEN,
+        Color.RED,
+        Color.YELLOW,
+        Color.CYAN,
+        )
     private var index = 0
+    private var cardNumber = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,11 +30,11 @@ class MainActivity : AppCompatActivity() {
             rv.layoutManager = LinearLayoutManager(this@MainActivity)
             rv.adapter = adapter
             btnAdd.setOnClickListener {
-                if (index == cardColor.size-1) {index = 0}
-                val card = Card(cardColor[index], "Карточка № $index")
-                Log.d("MYCARD", "index $index, color ${cardColor[index]}, R.color.purple ${R.color.purple_700}, k $k")
+                if (index > cardColor.size-1) {index = 0}
+               val card = Card(cardColor[index], "Карточка № $cardNumber")
                 adapter.addCard(card)
                 index++
+                cardNumber++
             }
         }
     }
