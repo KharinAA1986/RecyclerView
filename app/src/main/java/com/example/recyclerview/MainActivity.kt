06093ec927f38d7645f01b40213b13c35505c93c
KharinAA1986 <kharinaa1986@gmail.com>
2023-03-12 20:51:38 +0500

    We take colors from resources now!

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,37 +1,38 @@
 package com.example.recyclerview
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter = CardAdapter()
-    private val cardColor = listOf(
-        Color.WHITE,
-        Color.BLUE,
-        Color.GREEN,
-        Color.RED,
-        Color.YELLOW,
-        Color.CYAN,
-        )
     private var index = 0
     private var cardNumber = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
+         val cardColor = listOf(
+            ContextCompat.getColor(applicationContext,R.color.red),
+            ContextCompat.getColor(applicationContext,R.color.grey),
+             ContextCompat.getColor(applicationContext,R.color.green),
+            ContextCompat.getColor(applicationContext,R.color.purple_700),
+            ContextCompat.getColor(applicationContext,R.color.teal_700),
+             ContextCompat.getColor(applicationContext,R.color.purple_200),
+            ContextCompat.getColor(applicationContext,R.color.teal_200),
+        )
+        init(cardColor)
     }
-    private fun init() {
+    private fun init(cardColor: List<Int>) {
         binding.apply {
             rv.layoutManager = LinearLayoutManager(this@MainActivity)
             rv.adapter = adapter
             btnAdd.setOnClickListener {
                 if (index > cardColor.size-1) {index = 0}
-               val card = Card(cardColor[index], "Карточка № $cardNumber")
+               val card = Card(cardColor[index], "CARD № $cardNumber")
                 adapter.addCard(card)
                 index++
                 cardNumber++
