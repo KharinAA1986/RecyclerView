2a5b2ba1ae3a2d038fb8a4dc7e99219f7b10368f
KharinAA1986 <kharinaa1986@gmail.com>
2023-03-14 12:43:11 +0500

    Items in RecyclerView is clickable now

--- app/src/main/java/com/example/recyclerview/CardAdapter.kt
+++ app/src/main/java/com/example/recyclerview/CardAdapter.kt
@@ -6,16 +6,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview.databinding.CardItemBinding
 
-class CardAdapter: RecyclerView.Adapter<CardAdapter.CardHolder>() {
-    val cardList = ArrayList<Card>()
+class CardAdapter(val listener: Listener): RecyclerView.Adapter<CardAdapter.CardHolder>() {
+    private val cardList = ArrayList<Card>()
     class CardHolder (item: View): RecyclerView.ViewHolder(item) {
-        val binding = CardItemBinding.bind(item)
-        fun bind (card: Card) = with(binding){
+        private val binding = CardItemBinding.bind(item)
+        fun bind (card: Card,listener: Listener) = with(binding){
             tvTitle.text = card.title
             cv.setBackgroundColor(card.backGroundColor)
+            itemView.setOnClickListener{
+                listener.onClick(card)
+            }
         }
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item,parent,false)
         return CardHolder(view)
@@ -26,10 +28,13 @@ class CardAdapter: RecyclerView.Adapter<CardAdapter.CardHolder>() {
     }
 
     override fun onBindViewHolder(holder: CardHolder, position: Int) {
-        holder.bind(cardList[position])
+        holder.bind(cardList[position],listener)
     }
     fun addCard (card:Card){
         cardList.add(card)
         notifyDataSetChanged()
+    }
+    interface Listener {
+        fun onClick(card: Card)
     }
 }
--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CardAdapter.Listener {
     private lateinit var binding: ActivityMainBinding
-    private val adapter = CardAdapter()
+    private val adapter = CardAdapter(this)
     private var index = 0
     private var cardNumber = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +17,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
          val cardColor = listOf(
-            ContextCompat.getColor(applicationContext,R.color.red),
-            ContextCompat.getColor(applicationContext,R.color.grey),
-             ContextCompat.getColor(applicationContext,R.color.green),
-            ContextCompat.getColor(applicationContext,R.color.purple_700),
-            ContextCompat.getColor(applicationContext,R.color.teal_700),
-             ContextCompat.getColor(applicationContext,R.color.purple_200),
-            ContextCompat.getColor(applicationContext,R.color.teal_200),
+                ContextCompat.getColor(applicationContext,R.color.red),
+                ContextCompat.getColor(applicationContext,R.color.grey),
+                ContextCompat.getColor(applicationContext,R.color.green),
+                ContextCompat.getColor(applicationContext,R.color.purple_700),
+                ContextCompat.getColor(applicationContext,R.color.teal_700),
+                ContextCompat.getColor(applicationContext,R.color.purple_200),
+                ContextCompat.getColor(applicationContext,R.color.teal_200),
         )
         init(cardColor)
     }
@@ -38,5 +39,8 @@ class MainActivity : AppCompatActivity() {
                 cardNumber++
             }
         }
+    }
+    override fun onClick(card: Card) {
+        Toast.makeText(this,"Нажата ${card.title}",Toast.LENGTH_LONG).show()
     }
 }
